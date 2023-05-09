a55355c62d5d8a92ef37c487b7b8a81200b93ca5
Dave Jameson <davetrippinon@gmail.com>
2023-05-09 13:09:15 +0900

    created index and a show method

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: :show
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
